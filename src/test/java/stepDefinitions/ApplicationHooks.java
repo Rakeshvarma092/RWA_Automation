22ff51042aa3756b32ea2f466eebb0329f997a8c
rakesh.pericherla <rakesh.pericherla@toucanus.com>
2026-03-10 18:33:39 +0530

    Updated the Hooks Class

--- src/test/java/stepDefinitions/ApplicationHooks.java
+++ src/test/java/stepDefinitions/ApplicationHooks.java
@@ -1,32 +1,35 @@
 package stepDefinitions;
 
 //import DataBase.MongoDB;
-import DriverFactory.WebDriverFactory;
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.MediaEntityBuilder;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import ru.yandex.qatools.ashot.AShot;
-import ru.yandex.qatools.ashot.Screenshot;
-import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
-import utilities.ConfigReader;
-import utilities.ExtentReader;
-import utilities.MailReader;
-
-import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import DriverFactory.WebDriverFactory;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+import utilities.ConfigReader;
+import utilities.ExtentReader;
+import utilities.MailReader;
 
 public class ApplicationHooks extends WebDriverFactory {
     public static ExtentTest test;
@@ -140,5 +143,4 @@ public class ApplicationHooks extends WebDriverFactory {
         Date date = new Date();
         return dateFormat.format(date);
     }
-
 }
